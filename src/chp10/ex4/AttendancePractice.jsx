3fbf0266a08c269dce7be4233f569c0f7e96f58b
JeongYoungseo <wjddudtjjys@gmail.com>
2025-10-28 16:33:15 +0900

    AttendancePractice

--- src/chp10/ex4/AttendancePractice.jsx
+++ src/chp10/ex4/AttendancePractice.jsx
@@ -31,7 +31,6 @@ function AttendancePractice() {
                             </div>
                         </div>
                         )
-
                     )
                 }
             </div>
